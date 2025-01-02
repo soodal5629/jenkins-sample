8acf9387bd3e1ec5a4b18399364c6be305b8f324
jcl <chaelynjang@lotte.net>
2025-01-02 17:31:42 +0900

    젠킨스 샘플 프로젝트 구성

---
+++ src/main/java/com/example/jenkinssample/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.jenkinssample;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsSampleApplication.class);
+    }
+
+}
