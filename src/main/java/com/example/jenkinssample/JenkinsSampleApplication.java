88e0ae35ac65ed70c08752d39b9aa618d3818cff
jcl <chaelynjang@lotte.net>
2025-01-07 09:19:16 +0900

    톰캣서버에 배포 되는지 확인

--- src/main/java/com/example/jenkinssample/JenkinsSampleApplication.java
+++ src/main/java/com/example/jenkinssample/JenkinsSampleApplication.java
@@ -2,12 +2,18 @@ package com.example.jenkinssample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsSampleApplication {
+public class JenkinsSampleApplication extends SpringBootServletInitializer  {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsSampleApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsSampleApplication.class);
+    }
 }
